531ec97ffd8fdd63c74e0b92e3298bced3d22ef0
maqsud <example@gmail.com>
2022-04-19 18:39:46 +0500

    works

---
+++ script.js
@@ -0,0 +1,89 @@
+// var ism = 'maks'
+// console.log(ism);
+// console.log(typeof(ism));
+// var son =123
+// console.log(son);
+// console.log(typeof(son));
+// var bool = true
+// console.log(bool);
+// console.log(typeof(bool));
+// var str = '123'
+// console.log(str);
+// console.log(typeof(str));
+
+// let a = '123'
+// // let b = 123
+// // console.log(a !== b);
+// console.log(a);
+// a = "232"
+// console.log(a);
+
+// let salom = 'hello '
+// let ism = 'proweb'
+// let info = salom+ism 
+// // console.log(salom+ism);
+// console.log(info);
+// let a = 34
+// let b = 13
+// console.log(14%8);
+
+// console.log(5>=5);
+
+// let ism = prompt('ismingiz nima')
+// // let salom = 'hello '
+// // console.log(typeof(ism));
+// alert('hello '+ ism )
+
+// let ism = prompt('ismingiz')
+// let  yosh = prompt("yilingiz")
+// let  yil = prompt("hozirgi yil")
+// let a = 2000
+// let b = 2022
+// // console.log(b-a);
+// alert(ism+' yoshingiz '+ (b-a))
+
+let ism = prompt('ismingizni kiriting')
+console.log('mening ismim '+ ism);
+
+let yosh = prompt('yoshingizni kiriting')
+console.log('mening yoshim '+ yosh);
+
+let misol =prompt('misol yeching '+ '5-2=?' )
+console.log(  'misol 1: ' + '5-2=3');
+console.log(' sizning javob '+misol);
+
+ misol =prompt('misol yeching '+ '20-5=?' )
+console.log( 'misol 2: ' + ' 20-5=15 ') 
+console.log(' sizning javob '+misol);
+
+ misol =prompt('misol yeching '+ '15*2=?' )
+console.log('misol 3: ' + ' 15*2=30 ');
+console.log(' sizning javob '+misol);
+
+ misol =prompt('misol yeching '+ '8/4=?' )
+console.log('misol 4: ' + ' 8/4=2 ')
+console.log(' sizning javob '+misol);
+
+ misol =prompt('misol yeching '+ '10%3=?' )
+console.log('misol 5:'  + ' 10%3=1 ');
+console.log(' sizning javob '+misol);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
